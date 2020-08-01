d2b07bc7868efd903312ffcf41747fbb583182e4
toukii <toukii@yahoo.com>
2020-08-01 08:52:52 +0800

    pyhome

--- py.go
+++ py.go
@@ -4,7 +4,7 @@ package pysh
 import (
 	"encoding/json"
 	"errors"
-	"os"
+	// "os"
 	"os/exec"
 	"strings"
 	"text/template"
@@ -18,6 +18,7 @@ type resp [][]float64
 
 var (
 	module string
+	PyHome string
 )
 
 func Init(module_ string) {
@@ -29,12 +30,12 @@ func GoPyFuncV2(funcname string, args [][]float64, params map[string]int32) ([][
 
 	pyargs, err1 := marshal(args)
 	pyparams, err2 := marshal(params)
-	log.Infof("pyargs:%+v", pyargs)
-	log.Infof("pyparams:%+v", pyparams)
+	// log.Infof("pyargs:%+v", pyargs)
+	// log.Infof("pyparams:%+v", pyparams)
 	log.Infof("%+v %+v", err1, err2)
 
 	out, err := call(funcname, pyargs, pyparams)
-	log.Infof("out:%+v", out)
+	// log.Infof("out:%+v", out)
 	if err != nil {
 		log.Errorf("%+v", err)
 		return nil, err
@@ -46,7 +47,7 @@ func GoPyFuncV2(funcname string, args [][]float64, params map[string]int32) ([][
 		log.Errorf("err:%+v", err)
 		return nil, err
 	}
-	log.Infof("resp:%+v", r)
+	// log.Infof("resp:%+v", r)
 	return [][]float64(r), nil
 }
 
@@ -74,13 +75,81 @@ print("[resp]: %s" % (resp))`
 		return "", err
 	}
 	args := goutils.ToString(wr.Bytes())
-	log.Infof("%s", args)
+	// log.Infof("%s", args)
 
 	// out, err := exc.Bash(args).Do()
 	out, err := execute("python", "-c", args)
 
 	outstr := goutils.ToString(out)
-	log.Infof("outstr:%+v", outstr)
+	// log.Infof("outstr:%+v", outstr)
+
+	ss := strings.Split(outstr, "[resp]: ")
+	if len(ss) < 2 {
+		return "", errors.New(outstr)
+	}
+	return ss[1], nil
+}
+
+func GoPyFuncV4(funcname string, args interface{}, params string) (string, error) {
+	log.Debugf("GoPyFuncV3, %s", funcname)
+
+	pyargs, err1 := marshal(args)
+	pyparams, err2 := marshal(params)
+	// log.Infof("pyargs:%+v", pyargs)
+	// log.Infof("pyparams:%+v", pyparams)
+	log.Infof("%+v %+v", err1, err2)
+
+	out, err := calltrain(funcname, pyargs, pyparams)
+	// log.Infof("out:%+v", out)
+	return out, err
+}
+
+func GoPyFuncV3(funcname string, args [][]float64, params string) (string, error) {
+	log.Debugf("GoPyFuncV3, %s", funcname)
+
+	pyargs, err1 := marshal(args)
+	pyparams, err2 := marshal(params)
+	// log.Infof("pyargs:%+v", pyargs)
+	// log.Infof("pyparams:%+v", pyparams)
+	log.Infof("%+v %+v", err1, err2)
+
+	out, err := calltrain(funcname, pyargs, pyparams)
+	// log.Infof("out:%+v", out)
+	return out, err
+}
+
+func calltrain(funcname, input, params string) (string, error) {
+	fmtargs := `import {{.Module}};
+params = {{.Params}}
+I = {{.Input}}
+resp = {{.Module}}.{{.Funcname}}(I, params)
+print("[resp]: %s" % (resp))`
+
+	at, err := template.New("callpy").Parse(fmtargs)
+	if err != nil {
+		log.Errorf("%+v", err)
+		return "", err
+	}
+	data := map[string]string{
+		"Module":   module,
+		"Funcname": funcname,
+		"Input":    input,
+		"Params":   params,
+	}
+	wr := bytes.NewWriter(make([]byte, 0, 1024))
+	if err := at.Execute(wr, data); err != nil {
+		log.Errorf("%+v", err)
+		return "", err
+	}
+	args := goutils.ToString(wr.Bytes())
+	// log.Infof("%s", args)
+
+	// out, err := exc.Bash(args).Do()
+	out, err := execute("python", "-c", args)
+	// log.Infof("%+v", args)
+
+	outstr := goutils.ToString(out)
+	// log.Infof("outstr:%+v", outstr)
 
 	ss := strings.Split(outstr, "[resp]: ")
 	if len(ss) < 2 {
@@ -91,7 +160,12 @@ print("[resp]: %s" % (resp))`
 
 func execute(executer, c, args string) ([]byte, error) {
 	cmd := exec.Command(executer, c, args)
-	cmd.Dir = os.Getenv("PYTHONPATH")
+	// cmd.Dir = os.Getenv("PYTHONPATH")
+	// log.Infof("PYTHONPATH:%s", cmd.Dir)
+	if cmd.Dir == "" {
+		cmd.Dir = PyHome
+	}
+	// log.Infof("PYTHONPATH final: %s", cmd.Dir)
 	return cmd.CombinedOutput()
 }
 
